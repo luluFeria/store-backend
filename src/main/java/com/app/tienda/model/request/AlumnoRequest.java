e20f715b70575f396add74122e0205f1d3ba729f
luluFeria <stgferia@gmail.com>
2024-03-03 14:34:18 -0600

    refactor(model): add model to student

--- src/main/java/com/app/tienda/model/request/AlumnoRequest.java
+++ src/main/java/com/app/tienda/model/request/AlumnoRequest.java
@@ -1,2 +1,31 @@
-package com.app.tienda.model.request;public class AlumnoRequest {
+package com.app.tienda.model.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@ToString
+public class AlumnoRequest {
+
+  @NotBlank(message = "Nombre obligatorio")
+  @Size(min = 3, max = 20, message = "El nombre debe de contener de 3 a 20 caracteres")
+  private String name;
+
+  @NotBlank(message = "Apellido obligatorio")
+  private String firstName;
+
+  private String secondName;
+
+  @NotNull(message = "Edad obligatorio")
+  @Min(value=2, message = "La edad debe contener minimo 2 digitos")
+  private Integer age;
+
+  private String gender;
 }
--- src/main/java/com/app/tienda/model/request/PersonRequest.java
+++ src/main/java/com/app/tienda/model/request/PersonRequest.java
@@ -1,4 +1,61 @@
 package com.app.tienda.model.request;
 
-public class Person {
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class PersonRequest {
+  @NotBlank(message = "El nombre no puede estar en blanco")
+  @Size(min = 3, max = 20, message = "El nombre debe de contener de 3 a 20 caracteres")
+  private String name;
+
+  @NotBlank(message = "El apellido paterno no puede estar en blanco")
+  private String firstName;
+
+  private String secondName;
+
+  @NotBlank(message = "La edad no puede estar en blanco")
+  private String age;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getSecondName() {
+    return secondName;
+  }
+
+  public void setSecondName(String secondName) {
+    this.secondName = secondName;
+  }
+
+  public String getAge() {
+    return age;
+  }
+
+  public void setAge(String age) {
+    this.age = age;
+  }
+
+  @Override
+  public String toString() {
+    return "PersonRequest{" +
+            "name='" + name + '\'' +
+            ", firstName='" + firstName + '\'' +
+            ", secondName='" + secondName + '\'' +
+            ", age='" + age + '\'' +
+            '}';
+  }
 }
+
