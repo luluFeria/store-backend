349b2392725d56bfe8c1f536d14e4513707ebb63
luluFeria <stgferia@gmail.com>
2024-03-03 14:28:28 -0600

    refactor(repository): add student repository

--- src/main/java/com/app/tienda/repository/AlumnoRepository.java
+++ src/main/java/com/app/tienda/repository/AlumnoRepository.java
@@ -1,2 +1,9 @@
-package com.app.tienda.repository;public interface AlumnoRepository {
+package com.app.tienda.repository;
+
+import com.app.tienda.entity.AlumnoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlumnoRepository extends JpaRepository<AlumnoEntity, Long> {
 }
