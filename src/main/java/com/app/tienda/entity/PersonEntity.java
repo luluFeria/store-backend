3673f584d9b252073fe7e093391b9d59f08073e9
luluFeria <stgferia@gmail.com>
2024-03-03 14:36:37 -0600

    refactor(entity): add entity to student

--- src/main/java/com/app/tienda/entity/PersonEntity.java
+++ src/main/java/com/app/tienda/entity/PersonEntity.java
@@ -1,2 +1,58 @@
-package com.app.tienda.entity;public class PersonEntity {
+package com.app.tienda.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+  @Table(name="persons")
+  public class PersonEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String firstName;
+    private String secondName;
+    private String age;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getSecondName() {
+    return secondName;
+  }
+
+  public void setSecondName(String secondName) {
+    this.secondName = secondName;
+  }
+
+  public String getAge() {
+    return age;
+  }
+
+  public void setAge(String age) {
+    this.age = age;
+  }
 }
