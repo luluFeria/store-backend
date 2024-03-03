95874ed032c42f96d3558ec0a61cb11f53d8f077
luluFeria <stgferia@gmail.com>
2024-03-03 13:49:26 -0600

    refactor(controller): add student controller

--- src/main/java/com/app/tienda/controller/DirectorController.java
+++ src/main/java/com/app/tienda/controller/DirectorController.java
@@ -1,2 +1,86 @@
-package com.app.tienda.controller;public class DirectorController {
+package com.app.tienda.controller;
+
+import com.app.tienda.model.request.Director;
+import com.app.tienda.model.request.Policia;
+import com.app.tienda.service.IDirectorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/api/v1/directores")
+public class DirectorController {
+  private final Logger log = LoggerFactory.getLogger(this.getClass());
+  @Autowired
+  private IDirectorService directorService;
+
+  @GetMapping
+  public ArrayList<Director> getAll(){
+    log.info("Entrando al directorController - getAll");
+
+    //ArrayList<Director> listDirectors = directorService.getAll();
+    //    return listDirectors;
+
+    return directorService.getAll();
+  }
+
+  @PostMapping
+  private ResponseEntity<String> save(@RequestBody Director director) {
+    log.info("DirectorController - save");
+    log.info("El parametro es: {}", director);
+
+    Director directorAgregado = directorService.save(director);
+
+    return new ResponseEntity<>("Se agregado con exito el director", HttpStatus.CREATED);
+  }
+
+  @GetMapping("/{id}")
+  private ResponseEntity<Director> getById(@PathVariable Long id) {
+    log.info("Entrando en mi funcion getById");
+    log.info("El parametro es: {}", id);
+
+    Director directorEncontrado = directorService.getById(id);
+
+    if (directorEncontrado != null) {
+      return new ResponseEntity<>(directorEncontrado, HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+  }
+
+  @PutMapping("/{id}")
+  private ResponseEntity<String> update(@PathVariable Long id, @RequestBody Director director) {
+    log.info("Entrando a la funcion update");
+    log.info("Parametro recibido Path {}", id);
+    log.info("Parametro recibido Body {}", director);
+
+    Boolean directorUpdate = directorService.update(id, director);
+
+    if (directorUpdate) {
+      return ResponseEntity.ok("Se ha modificado el director con exito");
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND)
+           .body(("No se encontro el id" + id));
+    }
+  }
+
+  @DeleteMapping("/{id}")
+  private ResponseEntity<String> delete(@PathVariable Long id) {
+    log.info("Entrando a la funcion delete");
+    log.info("Parametro: {}" , id);
+
+    Boolean directorEliminado = directorService.delete(id);
+
+    if (directorEliminado) {
+      log.info("Se elimino correctamente");
+      return ResponseEntity.noContent().build();
+    } else {
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró el director con ID: " + id);
+    }
+  }
 }
--- src/main/java/com/app/tienda/controller/PoliciaController.java
+++ src/main/java/com/app/tienda/controller/PoliciaController.java
@@ -1,50 +1,49 @@
 package com.app.tienda.controller;
 
 import com.app.tienda.model.request.Policia;
-import com.app.tienda.model.request.Profesor;
+import com.app.tienda.service.IPoliciaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/policias")
 public class PoliciaController {
-
   private final Logger log = LoggerFactory.getLogger(this.getClass());
+  @Autowired
+  private IPoliciaService policiaService;
 
-  ArrayList<Policia> listaPolicias = new ArrayList<>();
+  // El autowired es el patron que se encarga de crear el objeto de la clase externa
+  // y suminstrarlo a la clase que lo requiera.
 
   @GetMapping
   public ArrayList<Policia> getAll() {
-    log.info("Entrando a la funcion lista Policia");
+    log.info("Entrando al policiaController - getAll");
 
-    return listaPolicias;
+    return policiaService.getAll();
   }
 
   @PostMapping
-  private ResponseEntity<String> save(@RequestBody Policia policia) {
-    log.info("Entrando a la funcion save()");
-    log.info("Mi parametro es: {}", policia);
+  private ResponseEntity<Policia> save(@RequestBody Policia policia) {
+    log.info("PoliciaController - save");
+    log.info("El parametro es: {}", policia);
 
-    listaPolicias.add(policia);
+    Policia policiaAgregado =  policiaService.save(policia);
 
-    return new ResponseEntity<>("Se ha agregado con exito el policia", HttpStatus.CREATED);
+    return new ResponseEntity<>(policiaAgregado, HttpStatus.CREATED);
   }
 
   @GetMapping("/{id}")
   private ResponseEntity<Policia> getById(@PathVariable Long id) {
-    log.info("Entrando a la funcion getById");
-    log.info("Parametro recibido {}", id);
+    log.info("Entrando a mi funcion getById");
+    log.info("parametro: {}", id);
 
-    Policia policiaEncontrado = listaPolicias.stream()
-     .filter(policia -> policia.getId() == id)
-     .findFirst()
-     .orElse(null);
+    Policia policiaEncontrado = policiaService.getById(id);
 
     if (policiaEncontrado != null) {
       return new ResponseEntity<>(policiaEncontrado, HttpStatus.OK);
@@ -54,48 +53,35 @@ public class PoliciaController {
   }
 
   @PutMapping("/{id}")
-  private ResponseEntity<String> update(@PathVariable Long id, @RequestBody Policia policia) {
-    log.info("Entrando a la funcion update");
-    log.info("Parametro recibido {}", id);
-    log.info("Parametro recibido {}", policia);
+  private ResponseEntity<String> update(@PathVariable Long id,
+                                        @RequestBody Policia policia
+  ) {
+    log.info("Funcion update");
+    log.info("Parametro path: {}", id);
+    log.info("Parametro body: {}", policia);
 
-    Optional<Policia> policiaOptional = listaPolicias.stream()
-       .filter(pol -> pol.getId() == id)
-       .findFirst();
+    Boolean policyUpdated = policiaService.update(id, policia);
 
-    if (policiaOptional.isPresent()) {
-      Policia policiaEncontrado = policiaOptional.get();
-
-      policiaEncontrado.setId(policia.getId());
-      policiaEncontrado.setName(policia.getName());
-      policiaEncontrado.setLastName(policia.getLastName());
-      policiaEncontrado.setSecondLastName(policia.getSecondLastName());
-      policiaEncontrado.setAge(policia.getAge());
-      policiaEncontrado.setEmail(policia.getEmail());
-      policiaEncontrado.setPhone(policia.getPhone());
-      policiaEncontrado.setAddress(policia.getAddress());
-
-      return ResponseEntity.ok(" Se ha actualizado con exito el policia");
+    if (policyUpdated) {
+      return ResponseEntity.ok("Se ha modificado el policia con exito");
     } else {
       return ResponseEntity.status(HttpStatus.NOT_FOUND)
-              .body("No se encontró el policia con ID: " + id + ". No se realizó ninguna actualización.");
+              .body(("No se encontro el id" + id));
     }
   }
 
   @DeleteMapping("/{id}")
   private ResponseEntity<String> delete(@PathVariable Long id) {
-    log.info("Entrando a la funcion delete");
-    log.info("Parametro recibido {}", id);
+    log.info("Metodo delete");
+    log.info("id: {}", id);
 
-    boolean policiaEliminado = listaPolicias.removeIf(pol -> pol.getId() == id);
-    log.info("Variable bolean {}", policiaEliminado);
+    Boolean policiaEliminado = policiaService.delete(id);
 
     if (policiaEliminado) {
+      log.info("Se elimino correctamente");
       return ResponseEntity.noContent().build();
     } else {
-      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontró el policia con ID: " + id);
+      return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No se encontro ningun policia con ID: " + id);
     }
   }
-
-
 }
