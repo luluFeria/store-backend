e4fca98fd09daab25917575d1388e65623e93ac5
luluFeria <stgferia@gmail.com>
2024-03-03 18:06:37 -0600

    refactor(DirectorEntity): add getters and setters

--- src/main/java/com/app/tienda/entity/DirectorEntity.java
+++ src/main/java/com/app/tienda/entity/DirectorEntity.java
@@ -1,2 +1,67 @@
-package com.app.tienda.entity;public class DirectorEntity {
+package com.app.tienda.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="directores")
+public class DirectorEntity implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String firstName;
+  private String secondName;
+  private Integer age;
+  private String gender;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getSecondName() {
+    return secondName;
+  }
+
+  public void setSecondName(String secondName) {
+    this.secondName = secondName;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
 }
