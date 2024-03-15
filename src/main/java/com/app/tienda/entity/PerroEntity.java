d2f92b458d906139fbe0eab902801cddd53989bb
luluFeria <stgferia@gmail.com>
2024-03-14 18:46:24 -0600

    refactor(entity): add PerroEntity

--- src/main/java/com/app/tienda/entity/PerroEntity.java
+++ src/main/java/com/app/tienda/entity/PerroEntity.java
@@ -1,2 +1,58 @@
-package com.app.tienda.entity;public class PerroEntity {
+package com.app.tienda.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="Perros")
+public class PerroEntity implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String breed;
+  private Integer age;
+  private String gender;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getBreed() {
+    return breed;
+  }
+
+  public void setBreed(String breed) {
+    this.breed = breed;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
 }
