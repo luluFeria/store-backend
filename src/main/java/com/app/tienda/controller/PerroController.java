2f0b0bc0b450adff72d638081084fe5300e5d21a
luluFeria <stgferia@gmail.com>
2024-03-15 21:18:54 -0600

    refactor(controller): add perro controller

--- src/main/java/com/app/tienda/controller/PerroController.java
+++ src/main/java/com/app/tienda/controller/PerroController.java
@@ -1,2 +1,9 @@
-package com.app.tienda.controller;public class PerroController {
+package com.app.tienda.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/perros")
+public class PerroController {
 }
