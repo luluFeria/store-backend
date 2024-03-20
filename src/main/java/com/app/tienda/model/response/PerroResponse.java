aae163d2a686a7bfdcc8a7254919ed1526e438c9
luluFeria <stgferia@gmail.com>
2024-03-19 20:18:47 -0600

    refactor(model): perro response

--- src/main/java/com/app/tienda/model/response/PerroResponse.java
+++ src/main/java/com/app/tienda/model/response/PerroResponse.java
@@ -1,4 +1,49 @@
 package com.app.tienda.model.response;
 
 public class PerroResponse {
+  private Long id;
+  private String name;
+  private String breed;
+  private Integer age;
+  private String gender;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getBreed() {
+    return breed;
+  }
+
+  public void setBreed(String breed) {
+    this.breed = breed;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
 }
