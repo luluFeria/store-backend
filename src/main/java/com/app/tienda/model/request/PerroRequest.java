a7c3132999b282a23bb33d62eb53e565292ccb82
luluFeria <stgferia@gmail.com>
2024-03-19 19:59:13 -0600

    refactor(model): add perro request validations

--- src/main/java/com/app/tienda/model/request/PerroRequest.java
+++ src/main/java/com/app/tienda/model/request/PerroRequest.java
@@ -1,4 +1,27 @@
 package com.app.tienda.model.request;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.*;
+
+@Getter
+@Setter
+@ToString
 public class PerroRequest {
+
+  @NotBlank(message = "Nombre obligatorio")
+  @Size(min = 3, max = 20, message = "El nombre debe de contener de 3 a 20 caracteres")
+  private String name;
+
+  @NotBlank(message = "Raza obligatoria")
+  private String breed;
+
+  @NotNull(message = "Edad obligatoria")
+  @Min(value = 1, message = "El valor debe ser igual o mayor que 1")
+  @Max(value = 20, message = "El valor debe ser igual o menor que 20")
+  private Integer age;
+
+  private String gender;
 }
