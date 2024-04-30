4faa06ee509d6c14638a650ec86924c65a58b596
luluFeria <stgferia@gmail.com>
2024-04-30 09:53:19 -0600

    feat(entity): add address entity

---
+++ src/main/java/com/app/tienda/entity/AddressEntity.java
@@ -0,0 +1,24 @@
+package com.app.tienda.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name="address")
+public class AddressEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String city;
+  private String municipality;
+  private String colony;
+  private String street;
+
+}
