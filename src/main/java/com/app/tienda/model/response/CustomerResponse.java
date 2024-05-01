a2a2fe8cd56a135c66f11ba9c118c1eb1481f075
luluFeria <stgferia@gmail.com>
2024-05-01 14:37:31 -0600

    refactor(model): add attributes to customer response

--- src/main/java/com/app/tienda/model/response/CustomerResponse.java
+++ src/main/java/com/app/tienda/model/response/CustomerResponse.java
@@ -1,2 +1,23 @@
-package com.app.tienda.model.response;public class CustomerResponse {
+package com.app.tienda.model.response;
+
+import com.app.tienda.entity.AddressEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class CustomerResponse {
+  private Long id;
+
+  private String name;
+
+  private AddressEntity address;
+
+  private String phone;
+
+  private String email;
+
+  private String gender;
 }
