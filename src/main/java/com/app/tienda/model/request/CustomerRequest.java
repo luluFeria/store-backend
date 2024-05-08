45e6891ca9ad31f386b820090a6fe3b7cc443c32
luluFeria <stgferia@gmail.com>
2024-05-07 18:58:45 -0600

    refactor(model): fix the import of my validations

--- src/main/java/com/app/tienda/model/request/CustomerRequest.java
+++ src/main/java/com/app/tienda/model/request/CustomerRequest.java
@@ -1,27 +1,25 @@
 package com.app.tienda.model.request;
 
-import com.app.tienda.entity.AddressEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 @ToString
 public class CustomerRequest {
-  @NotBlank(message = "El nombre no puede estar en blanco")
+
+  @NotBlank(message = "Nombre obligatorio")
   @Size(min = 3, max = 20, message = "El nombre debe de contener de 3 a 20 caracteres")
   private String name;
 
-  @NotBlank(message = "La dirección no puede estar en blanco")
-  private AddressEntity address;
+  private AddressRequest address;
 
-  @Pattern(regexp = "\\d{10}", message = "El número de teléfono debe tener 10 dígitos")
+  @Size(max = 10, message = " El número de teléfono debe tener como máximo 10 dígitos")
   private String phone;
 
   @Email(message = "El correo debe ser válido")
