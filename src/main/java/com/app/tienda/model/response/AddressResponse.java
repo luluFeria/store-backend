50cfe5264c9280c57b5f7b0735cf6c84da95ae0f
luluFeria <stgferia@gmail.com>
2024-05-07 18:21:35 -0600

    refactor(model): add attributes to address response

--- src/main/java/com/app/tienda/model/response/AddressResponse.java
+++ src/main/java/com/app/tienda/model/response/AddressResponse.java
@@ -1,2 +1,20 @@
-package com.app.tienda.model.response;public class AddressResponse {
+package com.app.tienda.model.response;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class AddressResponse {
+  private Long id;
+
+  private String city;
+
+  private String municipality;
+
+  private String colony;
+
+  private String street;
 }
