64c003a40b37f74ca6a3d2db31e13d48db0997eb
luluFeria <stgferia@gmail.com>
2024-05-18 17:42:28 -0600

    refactor(entity): add attributes to provider entity

--- src/main/java/com/app/tienda/entity/ProviderEntity.java
+++ src/main/java/com/app/tienda/entity/ProviderEntity.java
@@ -1,2 +1,35 @@
-package com.app.tienda.entity;public class ProviderEntity {
+package com.app.tienda.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name="providers")
+public class ProviderEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  private String firstName;
+
+  private String secondName;
+
+  @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+  @JoinColumn(name = "address_id", referencedColumnName = "id")
+  private AddressEntity address;
+
+  private String phone;
+
+  private String email;
+
+  private String gender;
 }
