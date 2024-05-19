4fbf1496ac9c4f768044f0d8d3b2cb1cffde8d75
luluFeria <stgferia@gmail.com>
2024-05-18 19:06:29 -0600

    refactor(model): add attributes to provider response

--- src/main/java/com/app/tienda/model/response/ProviderResponse.java
+++ src/main/java/com/app/tienda/model/response/ProviderResponse.java
@@ -1,2 +1,28 @@
-package com.app.tienda.model.response;public class ProviderResponse {
+package com.app.tienda.model.response;
+
+import com.app.tienda.entity.AddressEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ProviderResponse {
+
+  private Long id;
+
+  private String name;
+
+  private String firstName;
+
+  private String secondName;
+
+  private AddressEntity address;
+
+  private String phone;
+
+  private String email;
+
+  private String gender;
 }
