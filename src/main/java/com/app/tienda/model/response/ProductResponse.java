f4e6949857a60bd9c65cc7f89456afa9e637cfba
luluFeria <stgferia@gmail.com>
2024-06-04 12:34:05 -0600

    refactor(model): add attributes to product response

--- src/main/java/com/app/tienda/model/response/ProductResponse.java
+++ src/main/java/com/app/tienda/model/response/ProductResponse.java
@@ -1,2 +1,25 @@
-package com.app.tienda.model.response;public class ProductResponse {
+package com.app.tienda.model.response;
+
+import com.app.tienda.entity.ProviderEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ProductResponse {
+  private Long id;
+
+  private String name;
+
+  private String description;
+
+  private Integer price;
+
+  private Integer quantityInInventory;
+
+  private ProviderEntity provider;
+
+  private String category;
 }
