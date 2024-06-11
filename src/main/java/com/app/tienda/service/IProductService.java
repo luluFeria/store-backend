8956f50e5a9a869fefdccefa9ccd5b2cea74d6fd
luluFeria <stgferia@gmail.com>
2024-06-11 15:54:25 -0600

    feat(service): add product interface

---
+++ src/main/java/com/app/tienda/service/IProductService.java
@@ -0,0 +1,15 @@
+package com.app.tienda.service;
+
+import com.app.tienda.model.request.ProductRequest;
+import com.app.tienda.model.response.ProductResponse;
+
+import java.util.List;
+
+public interface IProductService {
+  public List<ProductResponse> findAll();
+  public ProductResponse save(ProductRequest productRequest);
+  public ProductResponse getById(Long id);
+
+  public ProductResponse update(Long id, ProductRequest productRequest);
+  public void delete(Long id);
+}
