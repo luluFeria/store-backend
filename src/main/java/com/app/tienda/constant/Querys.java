879232710f46a3be50c70c17f2d2d1106b431c4d
luluFeria <stgferia@gmail.com>
2024-06-26 20:30:48 -0600

    feat(constant): create querys class

---
+++ src/main/java/com/app/tienda/constant/Querys.java
@@ -0,0 +1,9 @@
+package com.app.tienda.constant;
+
+public class Message {
+  //Estoy definiendo una constante, porque viene la palabra final
+  public static final String ID_NOT_FOUND = "El id no existe";
+  public static final String SAVE_ERROR = "Se produjo un error al guardar ";
+  public static final String UPDATE_ERROR = "Se produjo un error al actualizar ";
+  public static final String DELETE_ERROR = "Se produjo un error al eliminar ";
+}
---
+++ src/main/java/com/app/tienda/model/response/IProductResponse.java
@@ -0,0 +1,2 @@
+package com.app.tienda.model.response;public interface IProductResponse {
+}
