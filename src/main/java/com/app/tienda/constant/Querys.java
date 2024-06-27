5d5b7eeda43d8003ae0995800325a8350123f612
luluFeria <stgferia@gmail.com>
2024-06-26 20:38:17 -0600

    refactor(constant): add custom queries

--- src/main/java/com/app/tienda/constant/Querys.java
+++ src/main/java/com/app/tienda/constant/Querys.java
@@ -1,9 +1,23 @@
 package com.app.tienda.constant;
 
-public class Message {
+public class Querys {
   //Estoy definiendo una constante, porque viene la palabra final
-  public static final String ID_NOT_FOUND = "El id no existe";
-  public static final String SAVE_ERROR = "Se produjo un error al guardar ";
-  public static final String UPDATE_ERROR = "Se produjo un error al actualizar ";
-  public static final String DELETE_ERROR = "Se produjo un error al eliminar ";
+  public static final String QUERY_FILTER_PRODUCTS_BY_PROVIDER = "SELECT \n" +
+          "    p.id,\n" +
+          "    p.name,\n" +
+          "    p.description,\n" +
+          "    p.price,\n" +
+          "    p.quantity_In_Inventory as inventory,\n" +
+          "    p.category,\n" +
+          "    pr.name AS provider\n" +
+          "FROM \n" +
+          "    products p\n" +
+          "INNER JOIN \n" +
+          "    providers pr ON  pr.id =   p.provider_id\n" +
+          "WHERE \n" +
+          "    pr.id = :id";
+  public static final String QUERY_FILTER_PRODUCTS_BY_NAME = "SELECT * FROM PRODUCTS WHERE name = :name";
+
+  public static final String QUERY_FILTER_PRODUCTS_BY_CATEGORY = "SELECT * FROM PRODUCTS WHERE category = :category";
+
 }
