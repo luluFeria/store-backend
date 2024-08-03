4c6b850f8b976726e075e5c2121c7b872669dbd0
luluFeria <stgferia@gmail.com>
2024-08-02 22:48:43 -0600

    git commit -m "refactor(constant): add custom queries"

--- src/main/java/com/app/tienda/constant/Querys.java
+++ src/main/java/com/app/tienda/constant/Querys.java
@@ -2,12 +2,18 @@ package com.app.tienda.constant;
 
 public class Querys {
   //Estoy definiendo una constante, porque viene la palabra final
+  public static final String QUERY_FIND_ALL_PROVIDER_ORDERS = "SELECT po.id as orderId, po.date, po.status, po.total_amount as totalAmount, \n" +
+          "p.id as productId, p.name as product, p.price, pop.quantity \n" +
+          "FROM PROVIDER_ORDERS po\n" +
+          "JOIN PROVIDER_ORDER_PRODUCT pop on po.id = pop.provider_order_id\n" +
+          "JOIN PRODUCTS p on pop.product_id = p.id";
+
   public static final String QUERY_FILTER_PRODUCTS_BY_PROVIDER = "SELECT \n" +
           "    p.id,\n" +
           "    p.name,\n" +
           "    p.description,\n" +
           "    p.price,\n" +
-          "    p.quantity_In_Inventory as inventory,\n" +
+          "    p.quantity as inventory,\n" +
           "    p.category,\n" +
           "    pr.name AS provider\n" +
           "FROM \n" +
@@ -16,6 +22,20 @@ public class Querys {
           "    providers pr ON  pr.id =   p.provider_id\n" +
           "WHERE \n" +
           "    pr.id = :id";
+
+  public static final String QUERY_FILTER_PROVIDER_ORDER_BY_PROVIDER_ID = "SELECT po.id AS orderId, po.date, po.status, po.total_amount AS totalAmount, \n" +
+    "p.id AS productId, p.name AS product, p.price, pop.quantity \n" +
+    "FROM PROVIDER_ORDERS po \n" +
+    "JOIN PROVIDER_ORDER_PRODUCT pop ON po.id = pop.provider_order_id \n" +
+    "JOIN PRODUCTS p ON pop.product_id = p.id \n" +
+    "WHERE po.provider_id = :providerId";
+
+  public static final String QUERY_FILTER_ORDERS_BY_STATUS = "SELECT po.id AS orderId, po.date, po.status, po.total_amount AS totalAmount, \n" +
+    "p.id AS productId, p.name AS product, p.price, pop.quantity \n" +
+    "FROM PROVIDER_ORDERS po \n" +
+    "JOIN PROVIDER_ORDER_PRODUCT pop ON po.id = pop.provider_order_id \n" +
+    "JOIN PRODUCTS p ON pop.product_id = p.id \n" +
+    "WHERE po.status = :status";
   public static final String QUERY_FILTER_PRODUCTS_BY_NAME = "SELECT * FROM PRODUCTS WHERE name = :name";
 
   public static final String QUERY_FILTER_PRODUCTS_BY_CATEGORY = "SELECT * FROM PRODUCTS WHERE category = :category";
