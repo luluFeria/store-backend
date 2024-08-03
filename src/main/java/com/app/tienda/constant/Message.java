ecc9fddcc10d2ec8fdc493804969c8c3400c358c
luluFeria <stgferia@gmail.com>
2024-08-02 22:46:48 -0600

    git commit -m "refactor(constant): add error and success messages"

--- src/main/java/com/app/tienda/constant/Message.java
+++ src/main/java/com/app/tienda/constant/Message.java
@@ -2,8 +2,11 @@ package com.app.tienda.constant;
 
 public class Message {
   //Estoy definiendo una constante, porque viene la palabra final
-  public static final String ID_NOT_FOUND = "El id no existe";
-  public static final String SAVE_ERROR = "Se produjo un error al guardar ";
-  public static final String UPDATE_ERROR = "Se produjo un error al actualizar ";
-  public static final String DELETE_ERROR = "Se produjo un error al eliminar ";
+  public static final String ID_NOT_FOUND = "Id not found";
+  public static final String SAVE_ERROR = "Error saving";
+  public static final String UPDATE_ERROR = "Error updating";
+  public static final String DELETE_ERROR = "Error deleting";
+  public static final String SAVE = "Added successfully";
+  public static final String UPDATE = "Updated successfully";
+  public static final String DELETE = "Deleted successfully";
 }
