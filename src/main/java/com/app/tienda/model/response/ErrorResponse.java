c94d52924aaa0b561fedfea173410c6054934ac8
luluFeria <stgferia@gmail.com>
2024-08-02 22:40:47 -0600

    git commit -m "feat(model): add error response"

---
+++ src/main/java/com/app/tienda/model/response/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.app.tienda.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+  private String error;
+
+  private String message;
+}
