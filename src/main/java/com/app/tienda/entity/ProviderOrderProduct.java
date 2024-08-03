0b8f4bb04ee110c6b3499f276c3be29aa9def9f2
luluFeria <stgferia@gmail.com>
2024-08-02 22:31:37 -0600

    git commit -m "feat(entity): add product order provider entity"

---
+++ src/main/java/com/app/tienda/entity/ProviderOrderProduct.java
@@ -0,0 +1,19 @@
+package com.app.tienda.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Getter
+@Setter
+@Embeddable
+public class ProviderOrderProduct {
+  @Column(name = "product_id")
+  private Long productId;
+
+  @Column(name = "quantity")
+  private Integer quantity;
+}
