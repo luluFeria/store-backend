54a46c2bd9c114b28c7694a87edbb08ed1161f58
luluFeria <stgferia@gmail.com>
2024-08-02 22:23:59 -0600

    git commit -m "refactor(service): add provider service implementation"

--- src/main/java/com/app/tienda/service/impl/ProviderServiceImpl.java
+++ src/main/java/com/app/tienda/service/impl/ProviderServiceImpl.java
@@ -2,12 +2,10 @@ package com.app.tienda.service.impl;
 
 import com.app.tienda.constant.Message;
 import com.app.tienda.entity.AddressEntity;
-import com.app.tienda.entity.CustomerEntity;
 import com.app.tienda.entity.ProviderEntity;
 import com.app.tienda.exception.InternalServerException;
 import com.app.tienda.exception.ResourceNotFoundException;
 import com.app.tienda.model.request.ProviderRequest;
-import com.app.tienda.model.response.CustomerResponse;
 import com.app.tienda.model.response.ProviderResponse;
 import com.app.tienda.repository.AddressRepository;
 import com.app.tienda.repository.ProviderRepository;
